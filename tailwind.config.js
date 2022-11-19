1b6054e6c0b35eb3077d7f62a1ea13f7ba812623
CofCat456 <a25690756@gmail.com>
2022-11-20 00:35:31 +0800

    feat: 🐈 add base tailwindcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6A33F8',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
